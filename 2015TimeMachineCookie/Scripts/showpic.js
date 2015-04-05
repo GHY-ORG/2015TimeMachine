3db8c815c8f81643eabf32195608b7e8d0e84075
cookie_xuan <cookie@ghy.cn>
2015-04-06 02:49:05 +0800

    Signed-off-by: cookie_xuan <cookie@ghy.cn>

---
+++ 2015TimeMachineCookie/Scripts/showpic.js
@@ -0,0 +1,57 @@
+﻿$(function () {
+    //分页
+    $('.pageli').click(function () {
+        showpicPost($(this).text());
+    });
+    //下拉菜单
+    $("[role='menuitem']").click(function () {
+        var select = $(this).attr('id');//时间顺序、时间倒序、热度最高
+    });
+    $('#index-search-btn').click(function () {
+        searchPost($('#index-search-input')[0].value);
+    });
+});
+
+function showpicPost(index) {
+    var sendData = { blockno: index };
+    if (sendData.blockno == "") {
+        return;
+    }
+    $.ajaxJsonPost({
+        url: "/Picture/Block",
+        data: JSON.stringify(sendData),
+        success: function () {
+        },
+        error: function () {
+            console.log("err.");
+        }
+    });
+}
+
+function orderPic(select,type) {
+    $.ajaxJsonPost({
+        url: "/Picture/Block",
+        data: JSON.stringify(sendData),
+        success: function () {
+        },
+        error: function () {
+            console.log("err.");
+        }
+    });
+}
+
+function searchPost(input) {
+    var sendData = { input: input };
+    if (sendData.input == "") {
+        return;
+    }
+    $.ajaxJsonPost({
+        url: "/Picture/Search",
+        data: JSON.stringify(sendData),
+        success: function () {
+        },
+        error: function () {
+            console.log("err.");
+        }
+    });
+}
