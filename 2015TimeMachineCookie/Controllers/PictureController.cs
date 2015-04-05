e4f8b81f4628b7bcde9fd1cbdfd3cd3f5b630f2c
zzq <zhzq326@163.com>
2015-04-05 12:06:02 +0800

    show pictures

--- 2015TimeMachineCookie/Controllers/PictureController.cs
+++ 2015TimeMachineCookie/Controllers/PictureController.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using System.Data;
+using Common;
+using Entity;
+using BADL;
+
 namespace _2015TimeMachineCookie.Controllers
 {
     public class PictureController : Controller
@@ -11,7 +16,17 @@ namespace _2015TimeMachineCookie.Controllers
         // GET: Picture
         public ActionResult Index()
         {
-            return View();
+            DataTable dt = BADL_showImg.showImg(1, 1);
+            return View(dt);
+        }
+
+        // Post: BlockNo
+        [HttpPost]
+        [Route("/picture/block/{blockno}")]
+        public ActionResult block(int blockno)
+        {
+            DataTable dt = BADL_showImg.showImg(1, blockno);
+            return View(dt);
         }
     }
 }
--- Entity/En_Img.cs
+++ Entity/En_Img.cs
@@ -8,7 +8,7 @@ namespace Entity
 {
     public class En_Img
     {
-        private Guid imgID;
+        private Guid imgID;//图片ID
 
         public Guid ImgID
         {
@@ -16,7 +16,7 @@ namespace Entity
             set { imgID = value; }
         }
 
-        private Guid uID;
+        private Guid uID;//作者ID
 
         public Guid UID
         {
@@ -24,7 +24,7 @@ namespace Entity
             set { imgID = value; }
         }
 
-        private string imgName;
+        private string imgName;//图片名称
 
         public string ImgName
         {
@@ -32,7 +32,7 @@ namespace Entity
             set { imgName = value; }
         }
 
-        private DateTime imgTime;
+        private DateTime imgTime;//拍摄时间
 
         public DateTime ImgTime
         {
@@ -40,14 +40,14 @@ namespace Entity
             set { imgTime = value; }
         }
 
-        private string imgLocation;
+        private string imgLocation;//拍摄地点
 
         public string ImgLocation
         {
             get { return imgLocation; }
             set { imgLocation = value; }
         }
-        private string imgPath;
+        private string imgPath;//图片路径
 
         public string ImgPath
         {
@@ -55,7 +55,7 @@ namespace Entity
             set { imgPath = value; }
         }
 
-        private int type;
+        private int type;//图片类型 1 照片组；2 故事组
 
         public int Type
         {
@@ -63,7 +63,7 @@ namespace Entity
             set { type = value; }
         }
 
-        private int sortOrder;
+        private int sortOrder;//故事组序号
 
         public int SortOrder
         {
@@ -71,7 +71,7 @@ namespace Entity
             set { sortOrder = value; }
         }
 
-        private string title;
+        private string title;//图片标题
 
         public string Title
         {
@@ -79,12 +79,28 @@ namespace Entity
             set { title = value; }
         }
 
-        private string description;
+        private string description;//图片描述
 
         public string Description
         {
             get { return description; }
             set { description = value; }
+        }
+
+        private int views;//浏览次数
+
+        public int Views
+        {
+            get { return views; }
+            set { views = value; }
+        }
+
+        private int votes;//投票数
+
+        public int Votes
+        {
+            get { return votes; }
+            set { votes = value; }
         }
 
         private int state;
@@ -97,7 +113,7 @@ namespace Entity
 
         public En_Img() { }
 
-        public En_Img(Guid _imgid, Guid _uid, string _imgname, string _imgpath, DateTime _imgTime, string _imgLocation, int _type, int _sortorder, string _title, string _description, int _state)
+        public En_Img(Guid _imgid, Guid _uid, string _imgname, string _imgpath, DateTime _imgTime, string _imgLocation, int _type, int _sortorder, string _title, string _description, int _views, int _votes, int _state)
         {
             imgID = _imgid;
             uID = _uid;
@@ -109,6 +125,8 @@ namespace Entity
             sortOrder = _sortorder;
             title = _title;
             description = _description;
+            views = _views;
+            votes = _votes;
             state = _state;
         }
     }
